426ca5e7d78223409a872d3b6b0f964b19a4bf39
Jagdeesh Lodhi <jagdeeshlodhi090@gmail.com>
2022-09-16 22:29:13 +0530

    Add files via upload

---
+++ Quick_Quize_chapter_05.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class Quick_Quize_chapter_05 {
+    public static void main(String[] args) {
+
+        Scanner src=new Scanner(System.in);
+//        int num=101;
+//        // quize no. 1
+//        while (num>100&&num<200){
+//            System.out.println(num);
+//            num++;
+//        }
+//
+//      //   Quize 2
+//        System.out.println("enter number : ");
+//        int num=src.nextInt();
+//        int a=1;
+//     do {
+//
+//         System.out.println(a);
+//         a++;
+//
+//     }while (a<=num);
+// // Quize No.3
+//        System.out.println("Enter Number: ");
+//        int num= src.nextInt();
+//        for (int i=1;i<=num;i++){
+//            if (i%2!=0) {
+//                System.out.println(i);
+//            }
+//        }
+    }
+
+}
