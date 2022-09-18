c4c1328861217601030bf05e4cc54ae12cafac75
Jagdeesh Lodhi <jagdeeshlodhi090@gmail.com>
2022-09-18 23:29:27 +0530

    Add files via upload

---
+++ Practice_Set_06.java
@@ -0,0 +1,66 @@
+import java.util.Scanner;
+public class Practice_Set_06 {
+    public static void main(String[] args) {
+        // q.1
+//        float sum=0f;
+//        float [] num={96.5f,98.5f,45.8f};
+//        for (int i=0;i<num.length;i++){
+//            sum+=num[i];
+//        }
+//        System.out.println(sum);
+        // q. 2
+//        int[] marks = {100, 95, 68, 67, 90};
+//        Scanner src=new Scanner(System.in);
+//        System.out.println("Enter A Number For cheaking element is present or not");
+//        int num= src.nextInt();
+//        if (num==marks[0]||num==marks[1]||num==marks[2]||num==marks[3]||num==marks[4]){
+//               System.out.println(num+" Present In Array");
+//           }else {
+//               System.out.println(num+" Not Present In Array");
+//           }
+        // q 3
+//        int sum=0;
+//        int []physics={95,48,33,78,66,35,35,65,99,99,99,99,99,99,99};
+//        System.out.print("element are : ");
+//         for (int element:physics){
+//             System.out.print(element+" ");
+//             sum+=element;
+//         }
+//        int avg=sum/ physics.length-1;
+//        System.out.println("\navrage marks is : "+avg);
+        // q. 4 addition of to array 2x3
+//        int [][]flats=new int[2][3];
+//        flats[0][0]=1;
+//        flats[1][0]=2;
+//        flats[0][1]=3;
+//        flats[1][1]=4;
+//        flats[0][2]=5;
+//        flats[1][2]=6;
+//        System.out.printf("%d %d %d\n%d %d %d",flats[0][0],
+//                flats[0][1],flats[0][2],flats[1][0],flats[1][1],flats[1][2]);
+//        int [][]build=new int[2][3];
+//        build[0][0]=1;
+//        build[1][0]=2;
+//        build[0][1]=3;
+//        build[1][1]=4;
+//        build[0][2]=5;
+//        build[1][2]=6;
+//        System.out.print("\n  +  \n");
+//        System.out.printf("%d %d %d\n%d %d %d",build[0][0],build[0][1],
+//                build[0][2],build[1][0],build[1][1],build[1][2]);
+//        System.out.print("\n  =  \n");
+//        System.out.printf("%d %d %d\n%d %d %d",build[0][0]+flats[0][0],
+//                build[0][1]+flats[0][1],build[0][2]+flats[0][2],
+//                build[1][0]+flats[1][0],build[1][1]+flats[1][1],
+//                build[1][2]+flats[1][2]);
+        // q.5
+//        int []marks={95,48,33,78,66,35,35,65,99,99,99,99,99,99,99};
+//        System.out.println("Reverse Order");
+//        for (int i=marks.length-1;i>=0 ;i--){
+//            System.out.print(marks[i]+" ");
+//        }
+    // q.6 miximum element in array
+        
+    }
+}
+
---
+++ Quick_Quize_Chapter_06.java
@@ -0,0 +1,50 @@
+public class Quick_Quize_Chapter_06 {
+    public static void main(String[] args) {
+        int[] marks = {100, 95, 68, 67, 90};
+        // quize no 1
+//        System.out.println("Printing a array");
+//        for (int i=0;i< marks.length;i++){
+//            System.out.print(marks[i]+" ");
+//        }
+//        // reverse order
+//        System.out.println();
+//        System.out.println("Reverse Order");
+//        for (int i=marks.length-1;i>=0 ;i--){
+//            System.out.print(marks[i]+" ");
+//        }
+//        System.out.println("Printing array using for each loop");
+//        for (int element:marks){
+//            System.out.print(element+" ");
+//        }
+//        System.out.println();
+//        System.out.println("Length of the array is");
+//        System.out.println(marks.length);
+//        System.out.println(marks[2]);
+        // quize practice of multi dimention array
+//        int [][]flats=new int[2][3];
+//        flats[0][0]=1;
+//        flats[1][0]=2;
+//        flats[0][1]=3;
+//        flats[1][1]=4;
+//        flats[0][2]=5;
+//        flats[1][2]=6;
+//        System.out.printf("%d %d %d\n%d %d %d",flats[0][0],
+//                flats[0][1],flats[0][2],flats[1][0],flats[1][1],flats[1][2]);
+//        int [][]build=new int[2][3];
+//        build[0][0]=1;
+//        build[1][0]=2;
+//        build[0][1]=3;
+//        build[1][1]=4;
+//        build[0][2]=5;
+//        build[1][2]=6;
+//        System.out.print("\n  +  \n");
+//        System.out.printf("%d %d %d\n%d %d %d",build[0][0],build[0][1],
+//                build[0][2],build[1][0],build[1][1],build[1][2]);
+//        System.out.print("\n  =  \n");
+//        System.out.printf("%d %d %d\n%d %d %d",build[0][0]+flats[0][0],
+//                build[0][1]+flats[0][1],build[0][2]+flats[0][2],
+//                build[1][0]+flats[1][0],build[1][1]+flats[1][1],
+//                build[1][2]+flats[1][2]);
+
+    }
+}
